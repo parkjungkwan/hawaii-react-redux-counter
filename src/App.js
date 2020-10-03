86bc689246898a98f6e67dd9df365a4d396b8c3f
pakjkwan <pakjkwan@gmail.com>
2020-10-03 23:29:21 +0900

    2020-10-03 23

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+//import Counter from './counter/Counter'
+import Counter from './counter/CounterContainer'
+
+const App = () => {
+  return <><Counter/></>
+}
+
+export default App;
